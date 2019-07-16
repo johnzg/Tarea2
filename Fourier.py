d761cff114019528918be696fd2a191b59f0ece1
johnzg <js.zarate@uniandes.edu.co>
2019-07-16 04:44:10 +0000

    programa py capa calcular la transformada sin exito

--- Fourier.py
+++ Fourier.py
@@ -16,9 +16,36 @@ def transformadaDeFourier2D(img):
     res=np.zeros((n,m))    
     for i in range(n):
         for j in range(m):
+            aux=0.0
             for k in range(n):
-                aux=np.sum(img[i,:]*(np.exp(1j*2*(np.pi)*(i*(k/n)+l*(j/m)))))
+                aux+=np.sum(img[i,:]*(np.exp(1j*2*(np.pi)*(i*(k/n)+l*(j/m)))))
             res[i,j]=aux            
     return res
 #print (transformadaDeFourier2D(seria))
 print(np.fft.fft2(seria))
+
+transSeria=np.fft.fft2(seria)
+transFeliz=np.fft.fft2(feliz)
+
+#sinValoresGrandes=np.where(np.abs(transImg)<30, transImg, 30 )
+
+transSeriaOrdenada=np.fft.fftshift(transSeria)
+transFelizOrdenada=np.fft.fftshift(transFeliz)
+
+#Graficando los espectros de la trasnformada de fourier de las dos imagenes
+plt.figure()
+plt.imshow(transSeriaOrdenada.real)
+plt.savefig("espectroFourierImagenSeria.png")
+
+plt.figure()
+plt.imshow(transFelizOrdenada.real)
+plt.savefig("espectroFourierImagenFeliz.png")
+
+#extrayendo la informacion deseada de las imagenes
+aux=np.zeros((254,170),dtype=bool)
+for i in range(254):
+    for j in range(170):
+        if(transImg[i,j] < 4000 ):
+            aux[i,j]=True
+infoSeria=np.where(aux ,transImg,0)
+
