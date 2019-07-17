54012ca3270c4d69c9cb8088332f38cbc289ceed
johnzg <js.zarate@uniandes.edu.co>
2019-07-17 00:55:59 +0000

    fourier con imagen Hibrida

--- Fourier.py
+++ Fourier.py
@@ -7,45 +7,74 @@ from scipy.fftpack import fft, fftfreq
 seria=plt.imread("cara_02_grisesMF.png")
 feliz=plt.imread("cara_03_grisesMF.png")
 
-#implementando la transformada de Fourier en 2D:
-
-def transformadaDeFourier2D(img):
-    n=img.shape[0]
-    m=img.shape[1]
-    l=np.array(range(m))
-    res=np.zeros((n,m))    
-    for i in range(n):
-        for j in range(m):
-            aux=0.0
-            for k in range(n):
-                aux+=np.sum(img[i,:]*(np.exp(1j*2*(np.pi)*(i*(k/n)+l*(j/m)))))
-            res[i,j]=aux            
-    return res
-#print (transformadaDeFourier2D(seria))
-print(np.fft.fft2(seria))
-
+#sacando la trasnformada de fourier y ordenandola
 transSeria=np.fft.fft2(seria)
 transFeliz=np.fft.fft2(feliz)
-
-#sinValoresGrandes=np.where(np.abs(transImg)<30, transImg, 30 )
 
 transSeriaOrdenada=np.fft.fftshift(transSeria)
 transFelizOrdenada=np.fft.fftshift(transFeliz)
 
 #Graficando los espectros de la trasnformada de fourier de las dos imagenes
 plt.figure()
-plt.imshow(transSeriaOrdenada.real)
-plt.savefig("espectroFourierImagenSeria.png")
 
-plt.figure()
-plt.imshow(transFelizOrdenada.real)
-plt.savefig("espectroFourierImagenFeliz.png")
+plt.subplot(1,2,1)
+plt.imshow(transSeriaOrdenada.real,vmin=-70,vmax=70)
+plt.title("Espectro Fourier Seria")
 
-#extrayendo la informacion deseada de las imagenes
+plt.subplot(1,2,2)
+plt.imshow(transFelizOrdenada.real,vmin=-70,vmax=70)
+plt.title("Espectro Fourier Feliz")
+
+plt.savefig("espectroFourierImagenes.png")
+
+#filtrando espectro de Fourier de las imagenes
 aux=np.zeros((254,170),dtype=bool)
 for i in range(254):
     for j in range(170):
-        if(transImg[i,j] < 4000 ):
+        if(abs(transSeria[i,j]) < 24.0 ):
             aux[i,j]=True
-infoSeria=np.where(aux ,transImg,0)
+transSeriaFiltrada=np.where(aux ,transSeria,0)
 
+aux=np.zeros((254,170),dtype=bool)
+for i in range(254):
+    for j in range(170):
+        if(abs(transFeliz[i,j]) > 16.0 ):
+            aux[i,j]=True          
+transFelizFiltrada=np.where(aux ,transFeliz,0)
+
+#Recuperando las imagenes filtradas a traves de la trasformada de Fourier inversa
+seriaFiltrada=np.fft.ifft2(transSeriaFiltrada)
+felizFiltrada=np.fft.ifft2(transFelizFiltrada)
+
+
+#Graficando las imagenes recuperadas
+
+plt.figure()
+
+plt.subplot(1,2,1)
+plt.axis('off')
+plt.imshow(-seriaFiltrada.real,cmap='Greys')
+plt.title("Seria Filtrada")
+
+plt.subplot(1,2,2)
+plt.axis('off')
+plt.imshow(-felizFiltrada.real,cmap='Greys')
+plt.title("Feliz Filtrada")
+
+plt.savefig("imagenesFiltradas.png")
+
+#Creando la imagen hibrida a partir de las imagenes filtradas anteriormente
+
+transHibrida=2.5*transSeriaFiltrada+0.7*transFelizFiltrada
+
+#Recuperando la imagen hibrida usando la transformada de Fourier inversa
+
+hibrida=np.fft.ifft2(transHibrida)
+
+#Graficando la imagen hibrida 
+
+plt.figure()
+plt.axis('off')
+plt.imshow(-hibrida.real,cmap='Greys')
+plt.title("Imagen Hibrida")
+plt.savefig("Hibrida.png")
