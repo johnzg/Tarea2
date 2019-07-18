153a16d074aea2a812b41cc7db7bbf03a8a9fdcf
johnzg <js.zarate@uniandes.edu.co>
2019-07-18 18:32:45 +0000

    grafica momento

--- Plots_hw2.py
+++ Plots_hw2.py
@@ -1,6 +1,94 @@
 import numpy as np
 import matplotlib.pylab as plt
 
+#Graficando el momento
+
+plt.figure(figsize=(14,14))
+
+data=np.genfromtxt("momento euler_dt1.dat")
+t=data[:,0]
+m=data[:,3]
+plt.subplot(3,3,1)
+plt.plot(t,m,color="blue")
+plt.title("Euler dt = 0.1 yr")
+plt.xlabel("t(anios)")
+plt.ylabel("momento")
+
+data=np.genfromtxt("momento euler_dt2.dat")
+t=data[:,0]
+m=data[:,3]
+plt.subplot(3,3,2)
+plt.plot(x,y,color="blue")
+plt.title("Euler dt = 0.1 yr")
+plt.xlabel("t(anios)")
+plt.ylabel("momento")
+
+data=np.genfromtxt("momento euler_dt3.dat")
+t=data[:,0]
+m=data[:,3]
+plt.subplot(3,3,3)
+plt.plot(t,m,color="blue")
+plt.title("Euler dt = 0.1 yr")
+plt.xlabel("t(anios)")
+plt.ylabel("momento")
+
+data=np.genfromtxt("momento leapFrog_dt1.dat")
+t=data[:,0]
+m=data[:,3]
+plt.subplot(3,3,4)
+plt.plot(t,m,color="red")
+plt.title("Leap Frog dt = 0.1 yr")
+plt.xlabel("t(anios)")
+plt.ylabel("momento")
+
+data=np.genfromtxt("momento leapFrog_dt2.dat")
+t=data[:,0]
+m=data[:,3]
+plt.subplot(3,3,5)
+plt.plot(t,m,color="red")
+plt.title("Leap Frog dt = 0.1 yr")
+plt.xlabel("t(anios)")
+plt.ylabel("momento")
+
+data=np.genfromtxt("momento leapFrog_dt3.dat")
+t=data[:,0]
+m=data[:,3]
+plt.subplot(3,3,6)
+plt.plot(t,m,color="red")
+plt.title("Leap Frog dt = 0.1 yr")
+plt.xlabel("t(anios)")
+plt.ylabel("momento")
+
+data=np.genfromtxt("momento rungeKutta_dt1.dat")
+t=data[:,0]
+m=data[:,3]
+plt.subplot(3,3,7)
+plt.plot(t,m,color="green")
+plt.title("Runge Kutta dt = 0.1 yr")
+plt.xlabel("t(anios)")
+plt.ylabel("momento")
+
+data=np.genfromtxt("momento rungeKutta_dt2.dat")
+t=data[:,0]
+m=data[:,3]
+plt.subplot(3,3,8)
+plt.plot(t,m,color="green")
+plt.title("Runge Kutta dt = 0.1 yr")
+plt.xlabel("t(anios)")
+plt.ylabel("y(AU)")
+
+data=np.genfromtxt("momento rungeKutta_dt3.dat")
+t=data[:,0]
+m=data[:,3]
+plt.subplot(3,3,9)
+plt.plot(t,m,color="green")
+plt.title("momento Runge Kutta dt = 0.1 yr")
+plt.xlabel("t(anios)")
+plt.ylabel("momento")
+
+plt.savefig("orbitas.png")
+
+#Graficando orbitas:
 
 plt.figure(figsize=(14,14))
 
@@ -84,6 +172,4 @@ plt.plot(x,y,color="green")
 plt.title("Runge Kutta dt = 0.1 yr")
 plt.xlabel("x(AU)")
 plt.ylabel("y(AU)")
-
-plt.savefig("resultado1")
-
+plt.savefig("momento.png")
