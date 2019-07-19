125ea437da533c928a442a6f02f5611fd12d5565
johnzg <js.zarate@uniandes.edu.co>
2019-07-19 04:33:38 +0000

    todo funciona bien aunque las graficas de energia se ven mas con los numeros que se sobreponen con otras graficas

--- Plots_hw2.py
+++ Plots_hw2.py
@@ -240,11 +240,11 @@ plt.savefig("VxVy_met_dt.png")
 
 #Graficando energia
 
-plt.figure(figsize=(14,14))
+plt.figure(figsize=(17,17))
 
 data=np.genfromtxt("euler_dt1.dat")
 t=data[:,0]
-u=data[:,5]
+u=data[:,4]
 plt.subplot(3,3,1)
 plt.plot(t,u,color="blue")
 plt.title("Euler dt = 0.75 yr")
@@ -252,7 +252,7 @@ plt.ylabel("energia(Msol*AU*AU/(anio*anio))")
 
 data=np.genfromtxt("euler_dt2.dat")
 t=data[:,0]
-u=data[:,5]
+u=data[:,4]
 plt.subplot(3,3,2)
 plt.plot(t,u,color="blue")
 plt.title("Euler dt = 0.01 yr")
@@ -260,7 +260,7 @@ plt.title("Euler dt = 0.01 yr")
 
 data=np.genfromtxt("euler_dt3.dat")
 t=data[:,0]
-u=data[:,5]
+u=data[:,4]
 plt.subplot(3,3,3)
 plt.plot(t,u,color="blue")
 plt.title("Euler dt = 0.001 yr")
@@ -268,7 +268,7 @@ plt.title("Euler dt = 0.001 yr")
 
 data=np.genfromtxt("leapFrog_dt1.dat")
 t=data[:,0]
-u=data[:,5]
+u=data[:,4]
 plt.subplot(3,3,4)
 plt.plot(t,u,color="red")
 plt.title("Leap Frog dt = 0.75 yr")
@@ -276,21 +276,21 @@ plt.ylabel("energia(Msol*AU*AU/(anio*anio))")
 
 data=np.genfromtxt("leapFrog_dt2.dat")
 t=data[:,0]
-u=data[:,5]
+u=data[:,4]
 plt.subplot(3,3,5)
 plt.plot(t,u,color="red")
 plt.title("Leap Frog dt = 0.01 yr")
 
 data=np.genfromtxt("leapFrog_dt3.dat")
 t=data[:,0]
-u=data[:,5]
+u=data[:,4]
 plt.subplot(3,3,6)
 plt.plot(t,u,color="red")
 plt.title("Leap Frog dt = 0.001 yr")
 
 data=np.genfromtxt("rungeKutta_dt1.dat")
 t=data[:,0]
-u=data[:,5]
+u=data[:,4]
 plt.subplot(3,3,7)
 plt.plot(t,u,color="green")
 plt.title("Runge Kutta dt = 0.75 yr")
@@ -299,7 +299,7 @@ plt.ylabel("energia(Msol*AU*AU/(anio*anio))")
 
 data=np.genfromtxt("rungeKutta_dt2.dat")
 t=data[:,0]
-u=data[:,5]
+u=data[:,4]
 plt.subplot(3,3,8)
 plt.plot(t,u,color="green")
 plt.title("Runge Kutta dt = 0.01 yr")
@@ -308,7 +308,7 @@ plt.xlabel("t(anio)")
 
 data=np.genfromtxt("rungeKutta_dt3.dat")
 t=data[:,0]
-u=data[:,5]
+u=data[:,4]
 plt.subplot(3,3,9)
 plt.plot(t,u,color="green")
 plt.title("Runge Kutta dt = 0.001 yr")
